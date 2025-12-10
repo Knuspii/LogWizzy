e9046fa985d796f2e57a27b2206f8c94f400c07e
Knuspii <220954979+Knuspii@users.noreply.github.com>
2025-12-10 20:02:45 +0100

    Update lin_build.sh

--- scripts/lin_build.sh
+++ scripts/lin_build.sh
@@ -5,6 +5,10 @@ echo "Updating Go modules..."
 go get -u ../.
 echo "Modules updated successfully."
 
+echo "Tidying up Go modules..."
+go mod tidy
+echo "Modules tidied successfully."
+
 echo "Formatting Go source files..."
 gofmt -s -w ../.
 echo "Formatting done."
@@ -16,9 +20,5 @@ echo "Linux amd64 build succeeded."
 echo "Building LogWizzy for Linux ARM64..."
 CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -o ../bin/logwizzy-arm64 ../.
 echo "Linux ARM64 build succeeded."
-
-echo "Tidying up Go modules..."
-go mod tidy
-echo "Modules tidied successfully."
 
 echo "All builds finished successfully."
